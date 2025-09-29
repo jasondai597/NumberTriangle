c673469775a19b3a68d120e214136b96001b3288
Jason Dai <jasondai@Jasons-MacBook-Air-9.local>
2025-09-29 16:39:44 -0400

    Implement NumberTriangle class' Todos

--- src/main/java/NumberTriangle.java
+++ src/main/java/NumberTriangle.java
@@ -63,7 +63,16 @@ public class NumberTriangle {
      * Note: a NumberTriangle contains at least one value.
      */
     public void maxSumPath() {
-        // for fun [not for credit]:
+        if (!isLeaf()) {
+            if (left != null) left.maxSumPath();
+            if (right != null) right.maxSumPath();
+
+            int leftSum = (left != null) ? left.getRoot() : 0;
+            int rightSum = (right != null) ? right.getRoot() : 0;
+            this.root += Math.max(leftSum, rightSum);
+            this.left = null;
+            this.right = null;
+        }// for fun [not for credit]:
     }
 
 
@@ -88,8 +97,19 @@ public class NumberTriangle {
      *
      */
     public int retrieve(String path) {
-        // TODO implement this method
-        return -1;
+        NumberTriangle current = this;
+        for (int i = 0; i < path.length(); i++) {
+            char c = path.charAt(i);
+            if (c == 'l') {
+                current = current.left;
+            } else if (c == 'r') {
+                current = current.right;
+            }
+            if (current == null) {
+                throw new IllegalArgumentException("Path leads to null node");
+            }
+        }
+        return current.getRoot();
     }
 
     /** Read in the NumberTriangle structure from a file.
@@ -109,9 +129,8 @@ public class NumberTriangle {
         InputStream inputStream = NumberTriangle.class.getClassLoader().getResourceAsStream(fname);
         BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
 
-
-        // TODO define any variables that you want to use to store things
-
+        NumberTriangle[][] allRows = new NumberTriangle[20][];
+        int currentRowIndex = 0;
         // will need to return the top of the NumberTriangle,
         // so might want a variable for that.
         NumberTriangle top = null;
@@ -122,9 +141,25 @@ public class NumberTriangle {
             // remove when done; this line is included so running starter code prints the contents of the file
             System.out.println(line);
 
-            // TODO process the line
-
-            //read the next line
+            String[] numberStrings = line.trim().split("\\s+");
+            NumberTriangle[] currentRow = new NumberTriangle[numberStrings.length];
+            for (int i = 0; i < numberStrings.length; i++) {
+                int value = Integer.parseInt(numberStrings[i]);
+                currentRow[i] = new NumberTriangle(value);
+            }
+            if (currentRowIndex == 0) {
+                top = currentRow[0];
+            }
+            allRows[currentRowIndex] = currentRow;
+            if (currentRowIndex > 0) {
+                NumberTriangle[] previousRow = allRows[currentRowIndex - 1];
+                for (int i = 0; i < previousRow.length; i++) {
+                    NumberTriangle parent = previousRow[i];
+                    parent.setLeft(currentRow[i]);
+                    parent.setRight(currentRow[i + 1]);
+                }
+            }
+            currentRowIndex++;
             line = br.readLine();
         }
         br.close();
